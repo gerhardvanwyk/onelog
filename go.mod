ec6f4e55669d9e97d16faf5462980762bdfb798c
Gerhard van Wyk <gerhard.vanwyk@vastech.co.za>
2020-01-09 09:45:32 +0200

    update mod

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/gerhardvanwyk/onelogplus
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/francoispqt/gojay v0.0.0-20181220093123-f2cc13a668ca
-	github.com/francoispqt/onelog v0.0.0-20190306043706-8c2bb31b10a4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.2.2
 )
 
--- onelog.go
+++ onelog.go
@@ -1,14 +1,14 @@
-// Package onelog is a fast, low allocation and modular JSON logger.
+// Package onelogplus is a fast, low allocation and modular JSON logger.
 //
 // It uses github.com/francoispqt/gojay as JSON encoder.
 //
 // Basic usage:
-// 	import "github.com/francoispqt/onelog/log"
+// 	import "github.com/gerhardvanwyk/onelogplus/log"
 //
 //	log.Info("hello world !") // {"level":"info","message":"hello world !", "time":1494567715}
 //
 // You can create your own logger:
-//	import "github.com/francoispqt/onelog
+//	import "github.com/gerhardvanwyk/onelogplus
 //
 //	var logger = onelog.New(os.Stdout, onelog.ALL)
 //
